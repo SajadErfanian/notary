1587615cbe6ef3200e6c18ef55ee427639e3a75b
numbeeer31 <numbeeer31@gmail.com>
2024-01-04 19:21:09 +0330

    shop

--- js/main.js
+++ js/main.js
@@ -1,117 +1,307 @@
-$(".Click-here").on('click', function () {
-    $(".custom-model-main").addClass('model-open');
-});
-$(".close-btn, .bg-overlay").click(function () {
-    $(".custom-model-main").removeClass('model-open');
-});
+(function ($) {
+    "use strict";
 
-const paginationNumbers = document.getElementById("pagination-numbers");
-const paginatedList = document.getElementById("paginated-list");
-const listItems = paginatedList.querySelectorAll("tr");
-const nextButton = document.getElementById("next-button");
-const prevButton = document.getElementById("prev-button");
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
 
-const paginationLimit = 10;
-const pageCount = Math.ceil(listItems.length / paginationLimit);
-let currentPage = 1;
 
-const disableButton = (button) => {
-    button.classList.add("disabled");
-    button.setAttribute("disabled", true);
-};
-
-const enableButton = (button) => {
-    button.classList.remove("disabled");
-    button.removeAttribute("disabled");
-};
-
-const handlePageButtonsStatus = () => {
-    if (currentPage === 1) {
-        disableButton(prevButton);
-    } else {
-        enableButton(prevButton);
-    }
-
-    if (pageCount === currentPage) {
-        disableButton(nextButton);
-    } else {
-        enableButton(nextButton);
-    }
-};
-
-const handleActivePageNumber = () => {
-    document.querySelectorAll(".pagination-number").forEach((button) => {
-        button.classList.remove("active");
-        const pageIndex = Number(button.getAttribute("page-index"));
-        if (pageIndex == currentPage) {
-            button.classList.add("active");
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
         }
     });
-};
+    $('.back-to-top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+        return false;
+    });
 
-const appendPageNumber = (index) => {
-    const pageNumber = document.createElement("button");
-    pageNumber.className = "pagination-number";
-    pageNumber.innerHTML = index;
-    pageNumber.setAttribute("page-index", index);
-    pageNumber.setAttribute("aria-label", "Page " + index);
 
-    paginationNumbers.appendChild(pageNumber);
-};
+    // Vendor carousel
+    $('.vendor-carousel').owlCarousel({
+        loop: true,
+        margin: 29,
+        nav: false,
+        autoplay: true,
+        smartSpeed: 1000,
+        responsive: {
+            0: {
+                items: 2
+            },
+            576: {
+                items: 3
+            },
+            768: {
+                items: 4
+            },
+            992: {
+                items: 5
+            },
+            1200: {
+                items: 6
+            }
+        }
+    });
 
-const getPaginationNumbers = () => {
-    for (let i = 1; i <= pageCount; i++) {
-        appendPageNumber(i);
+
+    // Related carousel
+    $('.related-carousel').owlCarousel({
+        loop: true,
+        margin: 29,
+        nav: false,
+        autoplay: true,
+        smartSpeed: 1000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            992: {
+                items: 4
+            }
+        }
+    });
+
+
+    // Product Quantity
+    $('.quantity button').on('click', function () {
+        var button = $(this);
+        var oldValue = button.parent().parent().find('input').val();
+        if (button.hasClass('btn-plus')) {
+            var newVal = parseFloat(oldValue) + 1;
+        } else {
+            if (oldValue > 0) {
+                var newVal = parseFloat(oldValue) - 1;
+            } else {
+                newVal = 0;
+            }
+        }
+        button.parent().parent().find('input').val(newVal);
+    });
+
+})(jQuery);
+var shoppingCart = (function () {
+
+    cart = [];
+
+    function Item(name, price, count) {
+        this.name = name;
+        this.price = price;
+        this.count = count;
     }
-};
 
-const setCurrentPage = (pageNum) => {
-    currentPage = pageNum;
+    // Save cart
+    function saveCart() {
+        localStorage.setItem('shoppingCart', JSON.stringify(cart));
+    }
 
-    handleActivePageNumber();
-    handlePageButtonsStatus();
+    // Load cart
+    function loadCart() {
+        cart = JSON.parse(localStorage.getItem('shoppingCart'));
+    }
+    if (localStorage.getItem("shoppingCart") != null) {
+        loadCart();
+    }
 
-    const prevRange = (pageNum - 1) * paginationLimit;
-    const currRange = pageNum * paginationLimit;
 
-    listItems.forEach((item, index) => {
-        item.classList.add("hidden");
-        if (index >= prevRange && index < currRange) {
-            item.classList.remove("hidden");
+    var obj = {};
+
+    // Add to cart
+    obj.addItemToCart = function (name, price, count) {
+        for (var item in cart) {
+            if (cart[item].name === name) {
+                cart[item].count++;
+                saveCart();
+                return;
+            }
         }
-    });
-};
-
-window.addEventListener("load", () => {
-    getPaginationNumbers();
-    setCurrentPage(1);
-
-    prevButton.addEventListener("click", () => {
-        setCurrentPage(currentPage - 1);
-    });
-
-    nextButton.addEventListener("click", () => {
-        setCurrentPage(currentPage + 1);
-    });
-
-    document.querySelectorAll(".pagination-number").forEach((button) => {
-        const pageIndex = Number(button.getAttribute("page-index"));
-
-        if (pageIndex) {
-            button.addEventListener("click", () => {
-                setCurrentPage(pageIndex);
-            });
+        var item = new Item(name, price, count);
+        cart.push(item);
+        saveCart();
+    }
+    // Set count from item
+    obj.setCountForItem = function (name, count) {
+        for (var i in cart) {
+            if (cart[i].name === name) {
+                cart[i].count = count;
+                break;
+            }
         }
-    });
+    };
+    // Remove item from cart
+    obj.removeItemFromCart = function (name) {
+        for (var item in cart) {
+            if (cart[item].name === name) {
+                cart[item].count--;
+                if (cart[item].count === 0) {
+                    cart.splice(item, 1);
+                }
+                break;
+            }
+        }
+        saveCart();
+    }
+
+    // Remove all items from cart
+    obj.removeItemFromCartAll = function (name) {
+        for (var item in cart) {
+            if (cart[item].name === name) {
+                cart.splice(item, 1);
+                break;
+            }
+        }
+        saveCart();
+    }
+
+    // Clear cart
+    obj.clearCart = function () {
+        cart = [];
+        saveCart();
+    }
+
+    // Count cart 
+    obj.totalCount = function () {
+        var totalCount = 0;
+        for (var item in cart) {
+            totalCount += cart[item].count;
+        }
+        return totalCount;
+    }
+
+    // Total cart
+    obj.totalCart = function () {
+        var totalCart = 0;
+        for (var item in cart) {
+            totalCart += cart[item].price * cart[item].count;
+        }
+        return Number(totalCart.toFixed(2));
+    }
+
+    // List cart
+    obj.listCart = function () {
+        var cartCopy = [];
+        for (i in cart) {
+            item = cart[i];
+            itemCopy = {};
+            for (p in item) {
+                itemCopy[p] = item[p];
+            }
+            itemCopy.total = Number(item.price * item.count).toFixed(2);
+            cartCopy.push(itemCopy)
+        }
+        return cartCopy;
+    }
+    return obj;
+})();
+
+
+// Add item
+$('.default-btn').click(function (event) {
+    // alert('working');
+    event.preventDefault();
+    var name = $(this).data('name');
+    var price = Number($(this).data('price'));
+    shoppingCart.addItemToCart(name, price, 1);
+    displayCart();
+});
+
+// Clear items
+$('.clear-cart').click(function () {
+    shoppingCart.clearCart();
+    displayCart();
 });
 
 
+function displayCart() {
+    var cartArray = shoppingCart.listCart();
+    var output = "";
+    for (var i in cartArray) {
+        output += "<tr>"
+            + "<td>" + cartArray[i].name + "</td>"
+            + "<td>(" + cartArray[i].price + ")</td>"
+            + "<td><div class='input-group'>"
+            + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
+            + "</div></td>"
+            + "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">x</button></td>"
+            + " = "
+            + "<td>" + cartArray[i].total + "</td>"
+            + "</tr>";
+    }
+    $('.show-cart').html(output);
+    $('.total-cart').html(shoppingCart.totalCart());
+    $('.total-count').html(shoppingCart.totalCount());
+}
 
+// Delete item button
 
+$('.show-cart').on("click", ".delete-item", function (event) {
+    var name = $(this).data('name')
+    shoppingCart.removeItemFromCartAll(name);
+    displayCart();
+})
 
+// Item count input
+$('.show-cart').on("change", ".item-count", function (event) {
+    var name = $(this).data('name');
+    var count = Number($(this).val());
+    shoppingCart.setCountForItem(name, count);
+    displayCart();
+});
+displayCart();
 
+//////// ui script start /////////
+// Tabs Single Page
+$('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
+$('.tab ul.tabs li a').on('click', function (g) {
+    var tab = $(this).closest('.tab'),
+        index = $(this).closest('li').index();
+    tab.find('ul.tabs > li').removeClass('current');
+    $(this).closest('li').addClass('current');
+    tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').slideUp();
+    tab.find('.tab_content').find('div.tabs_item:eq(' + index + ')').slideDown();
+    g.preventDefault();
+});
 
+// search function
+$('#search_field').on('keyup', function () {
+    var value = $(this).val();
+    var patt = new RegExp(value, "i");
 
+    $('.tab_content').find('.col-lg-3').each(function () {
+        var $table = $(this);
 
+        if (!($table.find('.featured-item').text().search(patt) >= 0)) {
+            $table.not('.featured-item').hide();
+        }
+        if (($table.find('.col-lg-3').text().search(patt) >= 0)) {
+            $(this).show();
+            document.getElementById('not_found').style.display = 'none';
+        } else {
+            document.getElementById("not_found").innerHTML = " Product not found..";
+            document.getElementById('not_found').style.display = 'block';
+        }
 
+    });
 
+});
